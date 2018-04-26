b8c4f15630c3daf597ae9f5e874474b8dd772a86
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-04-26 17:37:51 +0300

    Add assets, config.js, server.js, views

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'178832297916-uisr0goektdimjspdfrvlqoggs69evho.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET':'l7gk--4a40ZmfSbVHv6JoHEE',
+    'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+}
